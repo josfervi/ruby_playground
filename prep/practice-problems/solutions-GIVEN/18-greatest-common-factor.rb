20bb42e6ac9dd1b9717bb3f7f25ffe44dbe4b761
josfervi <jfv33@cornell.edu>
2016-11-14 22:05:57 +0000

    solved a few more problems

--- prep/practice-problems/solutions-GIVEN/18-greatest-common-factor.rb
+++ prep/practice-problems/solutions-GIVEN/18-greatest-common-factor.rb
@@ -30,10 +30,22 @@ puts(
   (greatest_common_factor(3, 9) == 3).to_s
 )
 puts(
+  'greatest_common_factor(3, 9): ' +
+  (greatest_common_factor(3, 9)).to_s
+)
+puts(
   'greatest_common_factor(16, 24) == 8: ' +
   (greatest_common_factor(16, 24) == 8).to_s
 )
 puts(
+  'greatest_common_factor(16, 24): ' +
+  (greatest_common_factor(16, 24)).to_s
+)
+puts(
   'greatest_common_factor(3, 5) == 1: ' +
   (greatest_common_factor(3, 5) == 1).to_s
+)
+puts(
+  'greatest_common_factor(3, 5): ' +
+  (greatest_common_factor(3, 5)).to_s
 )
